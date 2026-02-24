f364336aceae25a91e1c0ea675240e7d66b17922
Reid Mason <maddogshain132@gmail.com>
2026-02-24 23:38:29 +0000

    fix: added qemu guest agent

--- terraform/proxmox/nix-image/configuration.nix
+++ terraform/proxmox/nix-image/configuration.nix
@@ -3,14 +3,13 @@
     "${modulesPath}/profiles/qemu-guest.nix"
   ];
 
+  services.qemuGuest.enable = true;
+
   services.cloud-init.enable = true;
   services.cloud-init.network.enable = true;
 
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "prohibit-password";
-
-  # Temporary: allow console login for debugging
-  users.users.root.password = "nixos";
 
   system.stateVersion = "25.11";
 }
